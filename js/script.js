6dbbf6d82451a01133252dcd3b9fe3cebfc5f851
Elvin Samanov <samanov.elvin96@mail.ru>
2020-03-04 14:59:26 +0400

    add modal windiw

--- js/script.js
+++ js/script.js
@@ -81,4 +81,30 @@ window.addEventListener('DOMContentLoaded', function(){
 
     setClock('timer', deadline);
 
+
+
+    //Modal
+
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+
+  more.addEventListener('click', function() {
+      overlay.style.display = 'block';
+      this.classList.add('more-splash');
+      //otmena scroll
+      document.body.style.overflow = 'hidden';
+  });
+
+  close.addEventListener('click', function() {
+      overlay.style.display = 'none';
+      more.classList.remove('more-splash');
+      //dobavit-scroll
+      document.body.style.overflow = '';
+  })
+
 });
+
+
